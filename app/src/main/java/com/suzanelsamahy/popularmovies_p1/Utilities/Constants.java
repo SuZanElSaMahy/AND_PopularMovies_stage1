683fefdd9d1db10f8f1c975e165c9963398cd661
DESKTOP-6U55830\SuZan ElsaMahy <suzan@nweave.com>
2018-03-05 23:01:49 +0200

    - Removed api key.

--- app/src/main/java/com/suzanelsamahy/popularmovies_p1/Utilities/Constants.java
+++ app/src/main/java/com/suzanelsamahy/popularmovies_p1/Utilities/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String API_PARAM = "api_key";
     public static final String POPULAR_MOVIES_URL="movie/popular";
     public static final String TOP_MOVIES_URL="movie/top_rated";
-    public static final String API_KEY="a96dd50db1d44809af5e8e1af7e6e904";
+    public static final String API_KEY="";
     public static final String IMAGE_URL = "http://image.tmdb.org/t/p/w185" ;
 
 }
